334ac688d945fc2a40100c08a9cf540c61e62d2f
Chris Cheng <chrischeng@D-10-19-231-49.dhcp4.washington.edu>
2019-05-15 18:42:24 -0700

    complete chart 2 version 1.0

--- scripts/chart_two.R
+++ scripts/chart_two.R
@@ -1,1 +1,12 @@
 # Script for returning chart 2
+library("ggplot2")
+library("dplyr")
+library("tidyr")
+
+data <- read.csv("data/volcano_1980.csv", stringsAsFactors = FALSE)
+
+data_trim <- data %>%
+  filter(Year, VEI,TOTAL_DEATHS)
+
+total_death_vs_year <- ggplot(data_trim) +
+  geom_col(mapping = aes(x = Year, y = TOTAL_DEATHS, fill = VEI))
