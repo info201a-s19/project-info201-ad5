a7b9ee7c6f8e3ceeeade68ed128f5d3ee292268b
Chris Cheng <chrischeng@Chriss-MacBook-Pro.local>
2019-05-22 13:56:55 -0700

    complete charts & add charts to index

--- scripts/chart_three.R
+++ scripts/chart_three.R
@@ -1,1 +1,17 @@
 # Script for returning chart 3
+
+library("dplyr")
+library("ggplot2")
+
+top_five_country_greaest_damage <- function (data_set) {
+  data_trim <- data_set %>%
+    select(Country, VEI, TOTAL_DAMAGE_MILLIONS_DOLLARS) %>%
+    replace(is.na(.), 0) %>%
+    group_by(Country) %>%
+    summarize(total_damage = sum(TOTAL_DAMAGE_MILLIONS_DOLLARS)) %>%
+    top_n(5, total_damage) %>%
+    arrange(-total_damage)
+  
+  ggplot(data_trim, mapping = aes(x = Country, y = total_damage)) +
+    geom_point(mapping = aes(x = Country, y = total_damage))
+} 
--- scripts/chart_two.R
+++ scripts/chart_two.R
@@ -6,7 +6,9 @@ library("tidyr")
 total_death_vs_year <- function (data_set) {
   data_trim <- data_set %>%
     select(Year, VEI,TOTAL_DEATHS) %>%
-    replace(is.na(.), 0)
+    replace(is.na(.), 0) %>%
+    filter(Year > 1979 & Year < 2001)
+  
   ggplot(data_trim) +
     geom_col(mapping = aes(x = Year, y = TOTAL_DEATHS, fill = VEI))
 }
