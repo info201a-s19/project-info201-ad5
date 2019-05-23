86c3ac6a374bb6f7bf1351a81f36cc0b09ac3763
Yishuan Zhong <yasu720@uw.edu>
2019-05-22 19:58:28 -0700

    Fix all lint issues, re-knit html document

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -5,10 +5,12 @@ library(tidyr)
 # Out of the eruptions with recorded VEI's, what was the average for each year?
 # Also, what number of incidents per year did not have recorded incidents?
 get_table_info <- function(volcanos) {
-  vol_df <- drop_na(volcanos, Year) %>%
+  vol_df <- volcanos %>%
+    drop_na(Year) %>%
     group_by(Year) %>%
     summarise(Incidents = n())
-  vol_df_two <- drop_na(volcanos, VEI)
+  vol_df_two <- volcanos %>%
+    drop_na(VEI)
   vol_table <- vol_df_two %>%
     group_by(Year) %>%
     summarize(Incidents = n(), AVG_VEI = sum(VEI) / n())
@@ -17,8 +19,9 @@ get_table_info <- function(volcanos) {
   vol_table$Incidents.y[is.na(vol_table$Incidents.y)] <- 0
   vol_table <- vol_table %>%
     mutate("Missing incidents" = Incidents.x - Incidents.y)
-  vol_table <- rename(vol_table,
-                      "Number of Incidents with Recorded VEI" = Incidents.y)
-  vol_table <- rename(vol_table, "Total Number of Incidents" = Incidents.x)
+  names(vol_table)[names(vol_table) == "Incidents.y"] <-
+    "Number of Incidents with Recorded VEI"
+  names(vol_table)[names(vol_table) == "Incidents.x"] <-
+    "Total Number of Incidents"
   return(vol_table)
 }
