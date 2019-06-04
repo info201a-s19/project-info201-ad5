a2c70887c3fc7255ba6e5fc4a733dbed9ac75302
Yafei Wang <yw355@uw.edu>
2019-06-03 18:30:48 -0700

    worked on intro page and conclusion page

--- app_ui.R
+++ app_ui.R
@@ -5,16 +5,57 @@ library("shiny")
 # Application UI
 intro_page <- tabPanel(
   "Introduction Page",
-  titlePanel("Introduction Page"),
+  tags$h1("Introduction",
+          style = "font-family: cursive;
+          color: #4d3a7d;"
+  ),
   sidebarLayout(
-    sidebarPanel(),
-    mainPanel()
+    sidebarPanel(
+      tags$div(class = "info", checked = NA,
+        tags$p("Terminology: The Volcanic Explosivity Index (VEI) is a relative measure of the explosiveness of volcanic eruptions.")
+      )
+    ),
+    mainPanel(
+      # an introduction
+      tags$div(class = "introduction", checked = NA,
+        tags$p("As a group, we were interested in the various natural phenomena that occurs around us and decided to delve deeper
+                into an environmental structure that currently has tremendous impact on us in Seattle, Washington, and that would
+                be Mount Rainier. Some of us have taken/are currently taking a volcanos course and we believe this would be another
+                way to learn more about this topic, as it can greatly affect our awareness of volcanic activity. We source our volcano
+                data (from 1980 to 2019) from the"),
+        tags$a(href="https://www.ngdc.noaa.gov/nndc/servlet/ShowDatasets?dataset=102557&search_look=50&display_look=50", 
+                      "Significant Volcano Eruption Database"),
+        tags$hr(),
+
+        tags$p("Below is the three questions we are seeking to answer"),
+        
+        # List of 3 questions we are seeking to answer
+        tags$ol(
+          tags$li("Where did the eruptions occur from 1980 to 2019? We created a hazard map which includes all the volcanic eruptions
+                  occured from 1980 to 2019 around the world"), 
+          tags$li("What are different types of volcano in a country?"), 
+          tags$li("What is the most dangerous type of volcano? To answer this question, we created a pie chart
+                  and the dangerous level can be measured by number of death, number of people missing, number of people injured or the
+                  total damage in Dollars. The pie chart can include different types of volcano for comparison.")
+        ),
+        
+        # picture of a volcano
+        tags$img(src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Augustine_volcano_Jan_24_2006_-_Cyrus_Read.jpg/1200px-Augustine_volcano_Jan_24_2006_-_Cyrus_Read.jpg", width = "600px", height = "400px"),
+        style = "font-family: cursive;
+                color: #000000;
+                font-size: 18px;"
+      )
+
+    )
   )
 )
 
 chart_one <- tabPanel(
   "Hazard Map",
-  titlePanel("Hazard Map"),
+  tags$h1("Hazard Map",
+          style = "font-family: cursive;
+          color: #4d3a7d;"
+  ),
   sidebarLayout(
     sidebarPanel(
       
@@ -49,7 +90,10 @@ chart_two <- tabPanel(
 
 chart_three <- tabPanel(
   "Dangers of Volcano Types",
-  titlePanel("Dangers of Volcano Types"),
+  tags$h1("Dangers of Volcano Types",
+          style = "font-family: cursive;
+          color: #4d3a7d;"
+  ),
   sidebarLayout(
     sidebarPanel(
       selectInput(
@@ -80,10 +124,34 @@ chart_three <- tabPanel(
 
 summary_page <- tabPanel(
   "Conclusion Page",
-  titlePanel("Conclusion Page"),
+  tags$h1("conclusion",
+          style = "font-family: cursive;
+          color: #4d3a7d;"
+  ),
   sidebarLayout(
-    sidebarPanel(),
-    mainPanel()
+    sidebarPanel(
+      tags$div(class = "info", checked = NA,
+               tags$p("Info 201 Section AD Group 5
+                      Created By : Yishuan Chung & Chris Cheng & Yafei Wang")
+      )
+    ),
+    mainPanel(
+      tags$div(class = "conclusion", checked = NA,
+        tags$p("First takeaway: Based on the hazard map (measured by VEI) below, we found that volcanic eruptions usually occur along the coast of ", tags$strong("Pacific Ocean"),
+               ". Especially near south east asia and western caribbean."),
+        tags$img(src = 'hazardmap.png', width = "600px", height = "400px"),
+
+        tags$p("Second takeaway: "),
+        tags$img(),
+
+        tags$p("Thrid takeaway: The below pie chart (damage based on dollars) shows that ", tags$strong("Stratovolcano"), "is the
+               most dangerous type of volcano as it filled about 85% of the pie chart."),
+        tags$img(src = 'piechart.png', width = "500px", height = "400px"),
+        style = "font-family: cursive;
+                 color: #000000;
+                 font-size: 18px;"
+      )
+    )
   )
 )
 
